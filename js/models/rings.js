b80fa16992dbf1e9ced2ace273cdb2f7fe999a1a
Samuel Jackson <samueljackson@outlook.com>
2014-11-09 12:21:39 +0000

    Refs #14 Add config info for other planets.

--- js/models/rings.js
+++ js/models/rings.js
@@ -5,8 +5,8 @@ function Rings(creationParams)
 
 Rings.prototype.draw = function(modelViewMatrix)
 {
-    mat4.rotate(modelViewMatrix, modelViewMatrix, degToRad(45), [0,1,1]);
-    mat4.scale(modelViewMatrix, modelViewMatrix, [25,0.0,25]);
+    mat4.rotate(modelViewMatrix, modelViewMatrix, degToRad(-45), [0,1,1]);
+    mat4.scale(modelViewMatrix, modelViewMatrix, [150,0.0,150]);
 
     this.shaderUniforms.modelViewMatrix = modelViewMatrix;
     this.shaderUniforms.perspectiveMatrix = perspectiveMatrix;
